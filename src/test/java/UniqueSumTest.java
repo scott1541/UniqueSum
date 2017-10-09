f3830889d58d1de8264a39e78ad99eb54181828c
scott1541 <scott3933@gmail.com>
2017-10-09 16:47:54 +0100

    Update tests

--- src/test/java/UniqueSumTest.java
+++ src/test/java/UniqueSumTest.java
@@ -4,11 +4,35 @@ import org.junit.Test;
 
 public class UniqueSumTest{
 	@Test
-	public void test2(){
+	public void test1(){
 		UniqueSum sum1 = new UniqueSum();
 		assertEquals(3, sum1.method(1, 2, 2)) ;
 		
 		
 	}
+
+	@Test
+	public void test2(){
+		UniqueSum sum1 = new UniqueSum();
+		assertEquals(3, sum1.method(1, 2, 3)) ;
+		
+		
+	}
+	@Test
+	public void test3(){
+		UniqueSum sum1 = new UniqueSum();
+		assertEquals(10, sum1.method(1, 5, 4)) ;
+		
+		
+	}
+
+	@Test
+	public void test4(){
+		UniqueSum sum1 = new UniqueSum();
+		assertEquals(0, sum1.method(1, 1, 1)) ;
+		
+		
+	}
+
 	
 }
